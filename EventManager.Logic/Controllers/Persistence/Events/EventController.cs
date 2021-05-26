438d5de36b8208295e5cb84dfeabd6c9ca9697a8
Kemal Yavuz <miroyavuz@gmail.com>
2021-05-26 23:30:41 +0200

    Created a test Business logic

--- EventManager.Logic/Controllers/Persistence/Events/EventController.cs
+++ EventManager.Logic/Controllers/Persistence/Events/EventController.cs
@@ -1,6 +1,8 @@
 ﻿using EventManager.Contracts.Persistence.Events;
 using EventManager.Logic.Contracts;
 using EventManager.Logic.Entities.Events;
+using System;
+using System.Threading.Tasks;
 
 namespace EventManager.Logic.Controllers.Persistence.Events
 {
@@ -12,6 +14,28 @@ namespace EventManager.Logic.Controllers.Persistence.Events
 
 		public EventController(ControllerObject other) : base(other)
 		{
+		}
+
+		private void CheckEvent(IEvent item)
+        {
+			if (item.Title.Length > 256)
+            {
+				throw new Exception("Zu lang bro.");
+            }
+        }
+
+		public override Task<IEvent> InsertAsync(IEvent entity)
+        {
+			CheckEvent(entity);
+
+			return base.InsertAsync(entity);
+        }
+
+		public override Task<IEvent> UpdateAsync(IEvent entity)
+		{
+			CheckEvent(entity);
+
+			return base.UpdateAsync(entity);
 		}
 	}
 }
